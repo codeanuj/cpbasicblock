fce1680867baa9a614f43bc802565053b2a9b345
Anuj <80551063+codeanuj@users.noreply.github.com>
2021-04-04 13:53:34 +0530

    Update LargestRectangleInHistogramBruteForce.java

--- scaler/Stack/LargestRectangleInHistogramBruteForce.java
+++ scaler/Stack/LargestRectangleInHistogramBruteForce.java
@@ -1,33 +1,44 @@
-public class AbsoluteBruteForce { 
+//Largest rectangle in histogram
+//This is brute force but with better space complexity
+
+//The intution is to find the left boundry and right boundry of element i.e. till where it can expand
+
+
+public class LargestRectangleInHistogram { 
     public int largestRectangleArea(int[] A) {
         
         int max_area=0;
         
+        int left;
+        int right;
+        int curr_element;
+        int width;
+        int area;
         for(int i=0; i<A.length; i++){
-            int countleft=0;
-            int countright=0;
+            curr_element=A[i];
+            left=0;
+            right=0;
             
-            for(int j=i-1; j>=0; j--){
-                if(A[j]>A[i]){
-                    countleft++;
-                }else{
+            for(int j=i; j>=0; j--){
+                if(curr_element>A[j]){
+                    left =j+1;
                     break;
                 }
             }
             
-            for(int k=i+1; k<A.length; k++){
-                if(A[k]>A[i]){
-                    countright++;
-                }else{
+            for(int k=i;k<A.length; k++){
+                if(curr_element>A[k]){
+                    right=k-1;
                     break;
+                }
+                if(right==0 && k==A.length-1){
+                    right=k;
                 }
             }
             
-            int width=countleft+countright+1;
             
-            int area = A[i]*width;
-            
-            max_area= Math.max(area, max_area);
+            area=(right-left+1)*curr_element;
+            max_area= Math.max(area,max_area);
         }
         
         return max_area;
